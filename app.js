5d3dae8f58fdced9297a9ddf88f192b4f06537d8
Devansh Pandey <devanshpandeyji4321@gmail.com>
2025-07-28 00:04:19 +0530

    hhh

--- app.js
+++ app.js
@@ -1,4 +1,18 @@
+//SORRY FOR SPELLING MISTAKES IN COMMENTS 
+
 //GLOBALS --NO WINDOW
 //require -function to use modules(commonjs)
 //module -- info about current module
+
+//in node there is no window //remember this
+
+//GLOBAL VARIABLE-means anywhere in application i can access this variable
+
+console.log(__dirname)  //gives direcaty name
+setInterval(() => {
+    console.log("hello");
+},500)
+
+
+
 
